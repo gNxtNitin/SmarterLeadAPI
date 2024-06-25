14d1875b8388458c1b14846c53be3609e1c62493
Ashish Mishra <amishra@directscale.com>
2024-06-25 10:24:55 +0530

    Initial Commit

---
+++ SmarterLeadAPI/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SmarterLead.API.Models.RequestModel;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SmarterLead.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private IConfiguration _config;
+        public AdminController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpPost("AuthenticateUser")]
+        public IActionResult Login([FromBody] UserLogin model)
+        {
+            if (model.Username == "test" && model.Password == "password")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_config.GetValue<string>("jwt:key"));
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, model.Username)
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(2),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new { Token = tokenString });
+            }
+            return Unauthorized();
+        }
+        [Authorize]
+        [HttpGet("GetUserById")]
+        public IActionResult GetUserById( int userId)
+        {
+            return Ok("1");
+        }
+    }
+}
---
+++ SmarterLeadAPI/Controllers/LeadsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmarterLead.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadsController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
