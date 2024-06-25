9c90128edb155d0c38f48640420f3744aad057be
Ashish Mishra <amishra@directscale.com>
2024-06-25 10:38:47 +0530

    Initial commit 06/25

---
+++ SmarterLeadAPI/DataServices/DBManager.cs
@@ -0,0 +1,239 @@
+﻿using System.Data;
+using System.Text.Json;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Serilog;
+using System.Security.Cryptography;
+using ILogger = Serilog.ILogger;
+using Newtonsoft.Json;
+
+namespace SmarterLeadAPI.DataServices
+{
+    public class DBManager
+    {
+        private string connStr="";
+        private readonly ILogger _logger;
+        public static IConfiguration _config;
+        public DBManager(IConfiguration config,ILogger logger) 
+        {
+            _config = config;
+            _logger = logger;
+        }  
+        public DataTable GetDataTable(string query, string connStr)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    connection.Open();
+                    MySqlDataAdapter adap = new MySqlDataAdapter(command);
+                    adap.Fill(dt);
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error while running the query {query}\n Message: {ex.Message}\n Inner Exception: {ex.InnerException}\n Stack Trace: {ex.StackTrace}\n");
+            }
+            return dt;
+        }
+        public DataTable GetDataTable(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    connection.Open();
+                    MySqlDataAdapter adap = new MySqlDataAdapter(command);
+                    adap.Fill(dt);
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error while running the query {query}\n Message: {ex.Message}\n Inner Exception: {ex.InnerException}\n Stack Trace: {ex.StackTrace}\n");
+            }
+            return dt;
+        }
+        public DataSet GetDataSet(string query, string connStr)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    connection.Open();
+                    MySqlDataAdapter adap = new MySqlDataAdapter(command);
+                    adap.Fill(ds);
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error while running the query {query}\n Message: {ex.Message}\n Inner Exception: {ex.InnerException}\n Stack Trace: {ex.StackTrace}\n");
+            }
+            return ds;
+        }
+        public DataSet GetDataSet(string query)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    connection.Open();
+                    MySqlDataAdapter adap = new MySqlDataAdapter(command);
+                    adap.Fill(ds);
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error while running the query {query}\n Message: {ex.Message}\n Inner Exception: {ex.InnerException}\n Stack Trace: {ex.StackTrace}\n");
+            }
+            return ds;
+        }
+        public int ExecuteNonQuery(string query)
+        {
+            int result = 0;
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    connection.Open();
+                    result = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error while running the query {query}\n Message: {ex.Message}\n Inner Exception: {ex.InnerException}\n Stack Trace: {ex.StackTrace}\n");
+            }
+            return result;
+        }
+        public static string Serialize(object value)
+        {
+            return JsonConvert.SerializeObject(value).Replace("\\", "");
+        }
+        public string ConvertToJSON(DataSet dataSet)
+        {
+            int counter = 0;
+            var JSONString = new StringBuilder();
+            string json = string.Empty;
+            JSONString.Append("{");
+            foreach (DataTable table in dataSet.Tables)
+            {
+                counter++;
+                string tableName = table.TableName;
+                if (tableName == null || String.IsNullOrEmpty(tableName))
+                {
+                    tableName = "Table" + counter.ToString();
+                }
+                JSONString.Append("\"" + tableName + "\":");
+                JSONString.Append("[");
+                if (table.Rows.Count > 0)
+                {
+                    for (int i = 0; i < table.Rows.Count; i++)
+                    {
+                        JSONString.Append("{");
+                        for (int j = 0; j < table.Columns.Count; j++)
+                        {
+                            string rowData = table.Rows[i][j].ToString();
+                            StringWriter wr = new StringWriter();
+                            var jsonWriter = new JsonTextWriter(wr);
+                            jsonWriter.StringEscapeHandling = StringEscapeHandling.EscapeNonAscii;
+                            new Newtonsoft.Json.JsonSerializer().Serialize(jsonWriter, rowData);
+                            rowData = wr.ToString();
+                            if (j < table.Columns.Count - 1)
+                            {
+                                JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + rowData + ",");
+                            }
+                            else if (j == table.Columns.Count - 1)
+                            {
+                                JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + rowData);
+                            }
+                        }
+                        if (i == table.Rows.Count - 1)
+                        {
+                            JSONString.Append("}");
+                        }
+                        else
+                        {
+                            JSONString.Append("},");
+                        }
+                    }
+                }
+                else
+                {
+                    //JSONString.Append("{}");
+                }
+                if (counter == dataSet.Tables.Count)
+                {
+                    JSONString.Append("]");
+                }
+                else
+                {
+                    JSONString.Append("],");
+                }
+            }
+            JSONString.Append("}");
+            return JSONString.ToString();// JToken.Parse(JSONString.ToString()).ToString();
+        }
+        public string ConvertToJSON(DataTable table)
+        {
+            int counter = 0;
+            var JSONString = new StringBuilder();
+            string json = string.Empty;
+            JSONString.Append("{");
+            counter++;
+            string tableName = table.TableName;
+            if (tableName == null || String.IsNullOrEmpty(tableName))
+            {
+                tableName = "Table" + counter.ToString();
+            }
+            JSONString.Append("\"" + tableName + "\":");
+            JSONString.Append("[");
+            if (table.Rows.Count > 0)
+            {
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    JSONString.Append("{");
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        string rowData = table.Rows[i][j].ToString();
+                        StringWriter wr = new StringWriter();
+                        var jsonWriter = new JsonTextWriter(wr);
+                        jsonWriter.StringEscapeHandling = StringEscapeHandling.EscapeNonAscii;
+                        new Newtonsoft.Json.JsonSerializer().Serialize(jsonWriter, rowData);
+                        rowData = wr.ToString();
+                        if (j < table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + rowData + ",");
+                        }
+                        else if (j == table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + rowData);
+                        }
+                    }
+                    if (i == table.Rows.Count - 1)
+                    {
+                        JSONString.Append("}");
+                    }
+                    else
+                    {
+                        JSONString.Append("},");
+                    }
+                }
+            }
+            JSONString.Append("]}");
+            return JSONString.ToString();// JToken.Parse(JSONString.ToString()).ToString();
+        }
+    }
+}
---
+++ SmarterLeadAPI/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System.Text;
+// Add services to the container.
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("jwt:key"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+Log.Logger = new LoggerConfiguration().
+               WriteTo.File($"{ app.Configuration.GetValue<string>("LogPath")}//log.txt", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
